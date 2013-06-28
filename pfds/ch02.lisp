3b0824f8cde93ac4830705c8eafe03dd3a1e2995
Javier Olaechea <pirata@gmail.com>
2013-06-28 17:27:30 -0400

    Finished 2.1

--- pfds/ch02.lisp
+++ pfds/ch02.lisp
@@ -24,7 +24,7 @@
   (:documentation "Return a new stack which has the value at the index position substituting the old value."))
 
 
-(define-condition empty-stack (error)
+(define-condition empty-stack-condition (error)
   ((text :initarg :text :reader :text)))
 
 (define-condition invalid-subscript (error)
@@ -32,36 +32,41 @@
 
 ;;; Implementation
 
+(defclass empty-stack (stack)
+  ())
+
+(defmethod print-object ((obj empty-stack) stream)
+  "In order to help checking the results"
+  (format stream "Ø"))
+
+(defmethod empty-p ((stack empty-stack))
+  t)
+
+(defmethod head ((stack empty-stack))
+  (error 'empty-stack-condition :text "There is no tail, only ZUUL"))
+
+(defmethod tail ((stack empty-stack))
+  (error 'empty-stack-condition :text "There is no tail, only ZUUL"))
+
+
+;;; Stack
 (defclass stack ()
   ((head :initarg :head :initform nil)
    (tail :initarg :tail :initform nil)))
 
-;; Don't want the <STACK ... (obj address)> clutter in thi case
-;; (defmethod print-object ((obj stack) stream)
-;;   "In order to help checking the results"
-;;   (print-unreadable-object (obj stream :type t :identity t)
-;;     (with-slots (head tail) obj
-;;       (format stream "(~A, ~A)" head tail))))
-
 (defmethod print-object ((obj stack) stream)
   "In order to help checking the results"
   (with-slots (head tail) obj
-    (format stream "(~A, ~A)"
-            head
-            tail)))
+    (format stream "(~A, ~A)" head tail)))
 
-(defmethod empty ((stack stack))
-  '())
+(defmethod equal ((master stack) (candidate stack))
+  (cond ((empty)))
+  )
+(defgeneric equal ((stack stack) (stack stack)))
 
-(defun empty ()
-  "An alias basically"
-  (make-instance 'stack))
 
 (defmethod empty-p ((stack stack))
-  (with-slots (head tail) stack
-    (if (and (null head) (null tail))
-        t
-        nil)))
+  nil)
 
 (defmethod new-stack (head tail)
   (make-instance 'stack :head head :tail tail))
@@ -69,27 +74,24 @@
 
 (defmethod head ((stack stack))
   (with-slots (head) stack
-    (when (null head)
-      (error 'empty-stack :text "There is no head, only ZUUL"))
     head))
 
 (defmethod tail ((stack stack))
   (with-slots (tail) stack
-    (when (null tail)
-      (error 'empty-stack :text "There is no tail, only ZUUL"))
     tail))
 
 (defmethod merge-stacks ((left-stack stack) (right-stack stack))
-  (cond ((empty-p left-stack) right-stack)
-        (t
-         (new-stack
-          (head left-stack)
-          (merge-stacks (tail left-stack) right-stack)))))
+  (new-stack
+   (head left-stack)
+   (merge-stacks (tail left-stack) right-stack)))
+
+(defmethod merge-stacks ((left-stack empty-stack) (right-stack stack))
+   right-stack)
 
 (defmethod update ((stack stack) index value)
   (cond ((empty-p stack) (error 'invalid-subscript
-                                :text "Out of bounds error. LList not that long."))
-        ((= 0 index) (new-stack value stack))
+                                :text "Out of bounds error. List not that long."))
+        ((= 0 index) (new-stack value (tail stack)))
         (t (new-stack (head stack)
                       (update (tail stack) (- index 1) value)))))
 
@@ -97,19 +99,20 @@
 ;;; Exercise 2.1
 
 (defmethod suffixes ((stack stack))
-  (cond ((empty-p stack) stack)
-        (t (new-stack stack (suffixes (tail stack))))))
-
-(trace suffixes)
-(suffixes (new-stack 3 (new-stack 1 (new-stack 2 (make-instance 'stack)))))
-(new-stack 3 (new-stack 1 (new-stack 2 nil)))
-(transverse-stack (new-stack 3 (new-stack 1 (new-stack 2 (make-instance 'stack)))))
+  (cond ((empty-p stack) (list stack))
+        (t
+         (append (list stack) (suffixes (tail stack))))))
 
 
-(equal (new-stack 1 (new-stack 2 (make-instance 'stack)))
-       (new-stack 1 (new-stack 2 (make-instance 'stack))))
+
+;;; Extra
 
 (defmethod transverse-stack ((stack stack))
   (cond ((empty-p stack) (format t "~A~%" stack))
         (t (format t "~A~%" stack)
            (transverse-stack (tail stack)))))
+
+(defmethod copy-stack ((stack stack))
+  (cond ((empty-p stack) stack)
+        (t (new-stack (head stack)
+                      (copy-stack (tail stack))))))
