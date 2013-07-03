4b8ba0d7b019b4161463dd7b7a44110342d1e026
Javier Olaechea <pirata@gmail.com>
2013-07-02 22:01:56 -0400

    Insertion on the binary tree works!

--- src/binary-tree.lisp
+++ src/binary-tree.lisp
@@ -32,31 +32,40 @@
   (:documentation "Return a new tree, with the element inserted. If the node already exists then return the same tere."))
 
 (defclass node ()
-  ((element :initarg :element :initform nil :reader element)
+  ((element :initarg :element :initform 0 :reader element)
    (left-branch :initarg :left :initform nil :reader left-branch)
    (right-branch :initarg :right :initform nil :reader right-branch)))
 
 (defmethod empty? ((node node))
-  (element node))
+  nil)
 
 (defmethod member? (element (node node))
   (cond
-    ((empty? node) nil)
     ((less-than (element node) element) (member? element (left-branch node)))
     ((more-than (element node) element) (member? element (right-branch node)))
     (t t)))
 
 (defmethod insert (element (node node))
   (cond
-    ((empty? node) (make-instance 'node :element element))
     ((less-than element (element node))
-         (make-instance 'node
-                        :left (insert element (left-branch node))
-                        :element (element node)
-                        :right (right-branch node)))
-        ((more-than element (element node))
-                  (make-instance 'node
-                        :left (left-branch node)
-                        :element (element node)
-                        :right (insert element (right-branch node))))
-        (t node)))
+     (make-instance 'node
+                    :left (insert element (left-branch node))
+                    :element (element node)
+                    :right (right-branch node)))
+    ((more-than element (element node))
+     (make-instance 'node
+                    :left (left-branch node)
+                    :element (element node)
+                    :right (insert element (right-branch node))))
+    (t node)))
+
+(defparameter +empty-node+ (make-instance 'node))
+
+(defmethod empty? ((node (eql +empty-node+)))
+  t)
+
+(defmethod member? (element (node (eql +empty-node+)))
+  nil)
+
+(defmethod insert (element (node (eql +empty-node+)))
+  (make-instance 'node :element element :left +empty-node+ :right +empty-node+))
