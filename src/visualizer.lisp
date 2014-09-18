ed736ceabae3e078c5fcac70862a50ae28b49edd
Javier Olaechea <pirata@gmail.com>
2014-09-18 16:03:45 -0500

    WIP Visualizer

--- src/visualizer.lisp
+++ src/visualizer.lisp
@@ -12,6 +12,18 @@
                         :color *red*)
   (draw-string-solid (format nil "~A" element) (translate-point center :x -5 :y -10)))
 
+(defvar node-vertical-split 30)
+(defvar node-horizontal-split 30)
+
+(defun %node-horizontal-splits (node-levels current-split)
+  "Return a list of the horizontal distance between parents and children from root to leaves."
+  (cond
+    ((eql node-levels 1) (cons current-split nil))
+    (t (cons current-split
+             (%node-horizontal-splits (decf node-levels)
+                                      (+ (* 2 current-split)
+                                         node-horizontal-split))))))
+
 (defun draw-tree (tree)
   "First compute height and width, in levels of  nodes from the root..
 
@@ -22,33 +34,43 @@ Constants: node height and width separation, 70"
          (right-margin 30)
          (top-margin 30)
          (bottom-margin 30)
-         (node-vertical-split 30)
-         (node-horizontal-split 30)
+         (node-horizontal-splits (reverse
+                                 (%node-horizontal-splits
+                                  width-levels
+                                  node-vertical-split)))
          (window-height (+ top-margin bottom-margin
                            (* height-levels node-vertical-split)))
          (window-width (+ right-margin left-margin
-                          (* width-levels node-horizontal-split))
+                          (reduce #'+ node-horizontal-splits))
            )
-         (root-position (point :x (/ (- window-width left-margin right-margin)
-                                     2)
+         (root-position (point :x
+                               800
+                               ;; (/ (- window-width left-margin right-margin)
+                               ;;       2)
                                :y top-margin)))
-    (labels ((iter (node horizontal-offset vertical-offset)
-               (draw-node (element node) (point :x horizontal-offset
-                                                :y vertical-offset))
+    (labels ((iter (node horizontal-offset vertical-offset depth)
+               (draw-node (element node)
+                          (translate-point root-position
+                                           :x horizontal-offset
+                                           :y vertical-offset))
                (unless (empty? (left-branch node))
                  (iter (left-branch node)
                        (- horizontal-offset
-                          node-horizontal-split)
+                          (elt node-horizontal-splits depth))
                        (+ vertical-offset
-                          node-vertical-split)))
+                          node-vertical-split)
+                       (1+ depth)))
                (unless (empty? (right-branch node))
                  (iter (right-branch node)
                        (+ horizontal-offset
-                          node-horizontal-split)
+                          (elt node-horizontal-splits depth))
                        (+ vertical-offset
-                          node-vertical-split)))))
+                          node-vertical-split)
+                       (1+ depth)))))
       (with-init ()
-        (window window-width window-height
+        (window
+         ;; window-width window-height
+         1600 900
                 :title-caption "Node Visualizer"
                 :icon-caption "Node Visualizer")
 
@@ -56,5 +78,5 @@ Constants: node height and width separation, 70"
         (with-events ()
           (:quit-event () t)
           (:idle ()
-                 (iter tree (x root-position) (y root-position))
+                 (iter tree 0 0 0)
                  (update-display)))))))
